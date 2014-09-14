31eaddb680209c178e22270c5b021667bd46acdc
Sam Rad <sam.nikobonyadrad@rwth-aachen.de>
2014-09-14 12:44:48 +0200

    MessagePack functionality added
     - msgpack upgraded to 0.6.8 for Android compatibility
     - slf4j library added
     - ShamirGps class added

---
+++ preserv/src/main/java/de/rwth/comsys/samrad/preserv/model/ShamirGps.java
@@ -0,0 +1,139 @@
+package de.rwth.comsys.samrad.preserv.model;
+
+import mpc.ShamirSharing;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import org.msgpack.MessagePack;
+import org.msgpack.annotation.Message;
+
+/**
+ * Created by Sam on 8/18/2014.
+ */
+public class ShamirGps {
+
+    private final String RND_ALGORITHM   = "SHA1PRNG";
+
+    /**
+     * Creates the shares from given array.
+     * Format: result[p][i] -> 'i'th share for peer 'p'
+     *
+     * @param secrets: dealer's list of secret e.g. [0, 1, 0, 0, 0] for 5 polygons
+     * @param deg: polynomialDegree - at least t+1 shares are required
+     * @return shares: result[p][i]
+     * @throws Exception
+     */
+    public long[][] createShamirShares(int deg, int noOfPeers, long fieldSize, long[] secrets) {
+
+        try {
+
+            // Shamir initialization
+            ShamirSharing shamir = new ShamirSharing();
+            shamir.setRandomAlgorithm(RND_ALGORITHM);
+            shamir.setFieldSize(fieldSize);
+            shamir.setNumberOfPrivacyPeers(noOfPeers);
+            shamir.setDegreeT(deg);
+            shamir.init();
+
+            // Generate shares
+            return shamir.generateShares(secrets);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Pack shares for each privacy peer using MessagePack.
+     *
+     * @param shares, shares[p][i] is share i for privacy peer p
+     * @return messages, messages[i] holds shares for privacy peer i
+     */
+    public SharesMessage[] createMsgPackMessage(long[][] shares, long shamirModulus) {
+        SharesMessage[] messages = new SharesMessage[shares.length];
+        long random_id = Math.abs(new Random(System.currentTimeMillis()).nextLong());
+
+        for (int i = 0; i < shares.length; i++) {
+            // Create SharesMessage for privacy peer i
+            SharesMessage msg = new SharesMessage();
+            msg.id = random_id;
+            msg.modulus = shamirModulus;
+            msg.shares = shares[i];
+
+            // Add SharesMessage to result array
+            messages[i] = msg;
+        }
+        return messages;
+    }
+
+    /**
+     * Serialize messages using MessagePack and send them to the privacy peers.
+     *
+     * @param messages, the array containing the SharesMessages to be sent
+     * @param ppIPs, the array containing the IPs of the privacy peers
+     * @param ppPorts, the array containing the ports of the privacy peers
+     * @throws java.io.IOException
+     */
+    public boolean shareOut(SharesMessage[] messages, String[] ppIPs, int[] ppPorts) {
+
+        MessagePack msgpack = new MessagePack();
+
+        for(int i = 0; i < messages.length; i++) {
+            try{
+                // Put SharesMessage into a map for easier handling in Python code
+                Map<String, Object> msg = new HashMap<String, Object>();
+                msg.put("type", "inputshares");
+                msg.put("id", messages[i].id);
+                msg.put("peerID", i+1);
+                msg.put("m", messages[i].modulus);
+                msg.put("shares", messages[i].shares);
+
+                // Serialize message
+                byte[] bytes = msgpack.write(msg);
+
+                // Connect to privacy peer i
+                Socket sock = new Socket(ppIPs[i], ppPorts[i]);
+                BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
+                DataOutputStream out = new DataOutputStream(bos);
+//                DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+
+                // Send message
+                out.writeInt(bytes.length);
+                out.flush();
+                out.write(bytes);
+                out.flush();
+
+                System.out.println("Sent " + bytes.length + " bytes");
+
+                // Close connection
+                out.close();
+                sock.close();
+
+                Thread.sleep(1000);
+            } catch(IOException e) {
+
+                System.out.println("Error connecting to privacy peer " + i + ": " + e.getMessage());
+                return false;
+            } catch(InterruptedException e) {
+
+                System.out.println("Error in sleeping thread" + e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Message
+    public static class SharesMessage {
+        public String type = "inputshares";
+        public long id;
+        public long modulus;
+        public long[] shares;
+    }
+}
--- preserv/src/main/java/de/rwth/comsys/samrad/preserv/service/ConfigConstants.java
+++ preserv/src/main/java/de/rwth/comsys/samrad/preserv/service/ConfigConstants.java
@@ -3,18 +3,22 @@ package de.rwth.comsys.samrad.preserv.service;
 /**
  * Created by Sam on 9/7/2014.
  */
-public class RequestConstants {
+public class ConfigConstants {
 
     // Milliseconds per second
     private static final int MILLISECONDS_PER_SECOND = 1000;
     // Update frequency in seconds
-    private static final int UPDATE_INTERVAL_IN_SECONDS = 30;
+    private static final int UPDATE_INTERVAL_IN_SECONDS = 10;
     // Update frequency in milliseconds
     public static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
     // The fastest update frequency, in seconds
-    private static final int FASTEST_INTERVAL_IN_SECONDS = 30;
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 5;
     // A fast frequency ceiling in milliseconds
     public static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+    // Service pulse frequency in seconds
+    public static final long PULSE_INTERVAL_IN_SECOND = 1;
+    // Service pulse frequency in milliseconds
+    public static final long PULSE_INTERVAL = MILLISECONDS_PER_SECOND * PULSE_INTERVAL_IN_SECOND;
     // Stores the lat / long pairs in a text file
     public static final String LOCATION_FILE = "sdcard/location.txt";
     // Stores the connect / disconnect data in a text file
@@ -24,7 +28,7 @@ public class RequestConstants {
     /**
      * Suppress default constructor for noninstantiability
      */
-    private RequestConstants() {
+    private ConfigConstants() {
         throw new AssertionError();
     }
 }
